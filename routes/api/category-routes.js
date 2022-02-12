71bab5cd577ee31e6f985df54c85e192880e691e
Zachary Clark <zacclark369@gmail.com>
2022-02-12 11:47:27 -0800

    created catagory routes, exported to router

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,78 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const allCategories = await Catagory.findAll({
+    include: [{ model: Product }],
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
+  res.status(200).json(allCategories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const catagory = await Catgory.findByP(req.params.id, {
+    include: [{ model: Product }],
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
+
+  if (!catagory) {
+    res.status(404).json('no category found');
+  } else {
+    res.status(200).json(catagory);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  if(req.body.catagory_naem.length){
+    const newCatagory = await Catagory.create(req.body).catch((err) => {
+      res.status(400).json(err);
+    });
+    res.status(201).json(newCatagory);
+  } else {
+    res.status(404).json('no category found');
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const category = await Catagory.update(req.body, {
+    where: {id: req.params.id,
+    },
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
+
+  if (!category) {
+    res.status(404).json('no category found');
+  } else {
+    res.status(202).end();
+  };
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const destroyedCategory = await Catagory.destroy({
+    where: {id: req.params.id,
+    },
+  }).catch(err => {
+      res.status(500).json(err);
+    });
+
+    if(!destroyedCategory) {
+      res.status(404).json('no category found');
+    } else {
+      res.status(202).json('category deleted');
+    }
+  
 });
+
+module.exports = router;
 
 module.exports = router;
